31ab97564b6888c9fca7b42791ece948ff96824c
MuriloRohor <murilo.marquezini@gmail.com>
2023-10-05 19:05:19 -0300

    Schemas

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, status
+from fastapi import FastAPI, status, Depends 
 from sqlalchemy.orm import Session
 from src.schemas import schemas
 from src.infra.sqlalchemy.config.database import get_db, criar_bd
@@ -11,23 +11,23 @@ app = FastAPI()
 
 
 
-@app.post('/produtos')
-def criar_produto(produto: schemas.Produto, db: Session = Depends(get_db), status_code=status.HTTP_201_CREATED):
+@app.post('/produtos', status_code=status.HTTP_201_CREATED, response_model=schemas.ProdutoSimples)
+def criar_produto(produto: schemas.Produto, db: Session = Depends(get_db)):
     produto_criado = RepositorioProduto(db).Criar(produto)
     return produto_criado
 
 
-@app.get('/produtos')
-def listar_produtos(db: Session = Depends(get_db), status_code=status.HTTP_200_OK):
+@app.get('/produtos', status_code=status.HTTP_200_OK)
+def listar_produtos(db: Session = Depends(get_db)):
     produtos = RepositorioProduto(db).Listar()
     return produtos
 
-@app.post('/usuarios')
-def criar_usuario(usuario: schemas.User, db: Session = Depends(get_db), status_code=status.HTTP_201_CREATED):
+@app.post('/usuarios', status_code=status.HTTP_201_CREATED)
+def criar_usuario(usuario: schemas.User, db: Session = Depends(get_db)):
     usuario_criado = RepositorioUser(db).Criar(usuario)
     return usuario_criado
 
-@app.get('/usuarios')
-def listar_usuarios(db: Session = Depends(get_db), status_code=status.HTTP_200_OK):
+@app.get('/usuarios', status_code=status.HTTP_200_OK)
+def listar_usuarios(db: Session = Depends(get_db)):
     usuarios = RepositorioUser(db).Listar()
     return usuarios
--- src/schemas/schemas.py
+++ src/schemas/schemas.py
@@ -2,22 +2,28 @@ from pydantic import BaseModel
 from typing import Optional, List
 
 class User(BaseModel):
-    id: Optional[str] = None
+    id: Optional[int] = None
     nome: str
     telefone: str
 class Produto(BaseModel):
-    id: Optional[str] = None
+    id: Optional[int] = None
     nome: str
     detalhes: str
     preco: float
     disponivel: bool = False
+    class Config:
+        orm_mode = True
 
+class ProdutoSimples(BaseModel):
+    id: Optional[int] = None
+    nome: str
+    preco: float
     class Config:
         orm_mode = True
     
 
 class Pedido(BaseModel):
-    id: Optional[str] = None
+    id: Optional[int] = None
     quantidade: int
     entrega: bool = True
     endereco: str
