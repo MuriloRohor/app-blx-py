347f0ca2d978319abc40408c7de449a7d75bda7a
MuriloRohor <murilo.marquezini@gmail.com>
2023-10-05 08:47:42 -0300

    status code

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status
 from sqlalchemy.orm import Session
 from src.schemas import schemas
 from src.infra.sqlalchemy.config.database import get_db, criar_bd
@@ -12,22 +12,22 @@ app = FastAPI()
 
 
 @app.post('/produtos')
-def criar_produto(produto: schemas.Produto, db: Session = Depends(get_db)):
+def criar_produto(produto: schemas.Produto, db: Session = Depends(get_db), status_code=status.HTTP_201_CREATED):
     produto_criado = RepositorioProduto(db).Criar(produto)
     return produto_criado
 
 
 @app.get('/produtos')
-def listar_produtos(db: Session = Depends(get_db)):
+def listar_produtos(db: Session = Depends(get_db), status_code=status.HTTP_200_OK):
     produtos = RepositorioProduto(db).Listar()
     return produtos
 
 @app.post('/usuarios')
-def criar_usuario(usuario: schemas.User, db: Session = Depends(get_db)):
+def criar_usuario(usuario: schemas.User, db: Session = Depends(get_db), status_code=status.HTTP_201_CREATED):
     usuario_criado = RepositorioUser(db).Criar(usuario)
     return usuario_criado
 
 @app.get('/usuarios')
-def listar_usuarios(db: Session = Depends(get_db)):
+def listar_usuarios(db: Session = Depends(get_db), status_code=status.HTTP_200_OK):
     usuarios = RepositorioUser(db).Listar()
     return usuarios
